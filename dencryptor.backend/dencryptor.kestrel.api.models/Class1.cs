c5f073cfdee389b05a27d57c9c5763cf05336c61
Khondakar Readul Islam <khondakar22readul@gmail.com>
2022-03-09 10:12:18 +0100

    Initial commit

---
+++ dencryptor.backend/dencryptor.kestrel.api.models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace dencryptor.kestrel.api.models
+{
+    public class Class1
+    {
+    }
+}
---
+++ dencryptor.backend/dencryptor.kestrel.api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace dencryptor.kestrel.api
+{
+    public class Class1
+    {
+    }
+}
